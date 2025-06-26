ea0924b64db2b5df3805b5b49039e7f2fdde2d6e
fine-pine <lsm3645@g.skku.edu>
2025-06-26 21:38:32 +0900

    chore: init project

---
+++ src/test/kotlin/com/lineup/skku/SkkuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lineup.skku
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkkuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
